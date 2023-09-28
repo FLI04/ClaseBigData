78e239e0be46abb711e94f72cd4062ce79cb149c
Flavio Lopez <flavio.lopez35@uabc.edu.mx>
2023-09-27 19:53:31 -0700

    Practice 1 - Slice Luke word

--- Practices/practice1.scala
+++ Practices/practice1.scala
@@ -20,8 +20,13 @@ if (num %2 == 0){
 var bird = "tweet"
 println(s"Estoy escribiendo un $bird")
 
-//4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slilce para extraer la
+//4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slice para extraer la
 //   secuencia "Luke"
+
+var mensaje = "Hola Luke soy tu padre!"
+mensaje= mensaje slice (5,9)
+println(s"Hola $mensaje")
+
 //5. Cual es la diferencia entre value y una variable en scala?
 //6. Dada la tupla (2,4,5,1,2,3,3.1416,23) regresa el numero 3.1416 
 
