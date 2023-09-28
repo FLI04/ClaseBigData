3e5d949a83a9a996541f9c0455267f3b5c580ce1
Flavio Lopez <flavio.lopez35@uabc.edu.mx>
2023-09-27 19:39:11 -0700

    Practice 1 algorithm for even numbers

--- Practices/practice1.scala
+++ Practices/practice1.scala
@@ -1,6 +1,19 @@
 // Assessment 1/Practica 1
+
 //1. Desarrollar un algoritmo en scala que calcule el radio de un circulo
-//2. Desarrollar un algoritmo en scala que me diga si un numero es primo
+val pi = 3.1416
+val circle = pi * math.pow(4, 2) 
+println(s"Radio es:  $circle")
+
+//2. Desarrollar un algoritmo en scala que me diga si un numero es par
+
+val num = 13
+
+if (num %2 == 0){
+    println(s"El numero es par:  $num")
+ }else{
+    println(s"El numero no es par:  $num")}
+
 //3. Dada la variable bird = "tweet", utiliza interpolacion de string para
 //   imprimir "Estoy ecribiendo un tweet"
 //4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slilce para extraer la
@@ -9,6 +22,4 @@
 //6. Dada la tupla (2,4,5,1,2,3,3.1416,23) regresa el numero 3.1416 
 
 
-val pi = 3.1416
-val circle = pi * math.pow(4, 2) 
-println(s"Radio es:  $circle")
+
