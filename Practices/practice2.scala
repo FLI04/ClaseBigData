6a7140eb04c5183facf7d9f5671cd1f2c16e79b6
FLI04 <ledezmaj@uabc.edu.mx>
2023-10-01 07:51:24 -0700

    Adding more colors to list lista

--- Practices/practice2.scala
+++ Practices/practice2.scala
@@ -1,6 +1,6 @@
 //Desarrollar los siguientes puntos con lenguaje Scala y seguir el Git Flow Básico con su compañero de equipo:
-//1. Crea una lista llamada "lista" con los elementos "rojo", "blanco", "negro"
-//2. Añadir 5 elementos mas a "lista" "verde" ,"amarillo", "azul", "naranja", "perla"
+
+
 //3. Traer los elementos de "lista" "verde", "amarillo", "azul"
 //4. Crea un arreglo de numero en rango del 1-1000 en pasos de 5 en 5
 //5. Cuales son los elementos únicos de la lista Lista(1,3,3,4,6,7,3,7) utilice conversión a conjuntos
@@ -9,4 +9,8 @@
 //6 a . Imprime todas la llaves del mapa
 //6 b . Agrega el siguiente valor al mapa("Miguel", 23)
 
+//1. Crea una lista llamada "lista" con los elementos "rojo", "blanco", "negro"
 val lista = List("Rojo","Blanco","Negro")
+
+//2. Añadir 5 elementos mas a "lista" "verde" ,"amarillo", "azul", "naranja", "perla"
+val lista = List ("verde","amarillo", "azul", "naranja", "perla")
