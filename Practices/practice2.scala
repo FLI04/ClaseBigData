37ae60af89deb982969302708ae5e7bc8b2d7cad
Flavio Lopez <flavio.lopez35@uabc.edu.mx>
2023-10-01 09:02:16 -0700

    Create list and get unique values

--- Practices/practice2.scala
+++ Practices/practice2.scala
@@ -3,7 +3,7 @@
 
 
 
-//5. Cuales son los elementos únicos de la lista Lista(1,3,3,4,6,7,3,7) utilice conversión a conjuntos
+
 //6. Crea una mapa mutable llamado nombres que contenga los siguiente
 //     "Jose", 20, "Luis", 24, "Ana", 23, "Susana", "27"
 //6 a . Imprime todas la llaves del mapa
@@ -21,4 +21,9 @@ updatedList slice (3, 6)
 
 //4. Crea un arreglo de numero en rango del 1-1000 en pasos de 5 en 5
 val arr = Array.range(1,1000,5)
+
+//5. Cuales son los elementos únicos de la lista Lista(1,3,3,4,6,7,3,7) utilice conversión a conjuntos
+val Lista = List (1,3,3,4,6,7,3,7)
+val unicosLista = Lista.distinct
+Lista.toSet
 
