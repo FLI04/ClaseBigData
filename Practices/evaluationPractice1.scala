ad2f096d062d7e4eb9a90802f12fc4eb866020c5
Flavio Lopez <flavio.lopez35@uabc.edu.mx>
2023-10-22 08:14:36 -0700

    7.- Create HVRatio dataframe adding HV Ratio Column with Volume and High relation

--- Practices/evaluationPractice1.scala
+++ Practices/evaluationPractice1.scala
@@ -17,3 +17,9 @@ df.head(5)
 
 // 6. Usa el método describe () para aprender sobre el DataFrame.
 df.describe().show()
+
+//7. Crea un nuevo Dataframe a partir del df creado anteriormente llamdo “HVRatio” para crear una columna nueva llamada “HV Ratio” que es la relación que existe
+//  entre el precio de la columna “High” frente a la columna “Volumen” de acciones negociadas por un día. Hint - es una operación
+
+val HVRatio = df.withColumn("HV Ratio", $"Volume" / $"High")
+HVRatio.show()
